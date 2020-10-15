78a7adf148ea4f19d7af563e47101b4c704bc989
Svend Sorensen <svend@svends.net>
2020-10-15 07:54:45 -0700

    Fix

--- overlays/rustEnv.nix
+++ overlays/rustEnv.nix
@@ -16,7 +16,7 @@ final: prev:
       rustc
       rustfmt
       rust-analyzer # collision with rust from nixpkgs-mozilla
-    ] ++ lib.optionals stdenv.isDarwin [
+    ] ++ lib.optionals stdenv.isLinux [
       # FTB on darwin: https://github.com/NixOS/nixpkgs/pull/100289
       cargo-edit # cargo add/rm/upgrade (https://github.com/killercup/cargo-edit)
      ];
