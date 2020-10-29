059ab8cf639383b2b6023d7d247fe63c1584aed1
Svend Sorensen <svend@svends.net>
2020-10-29 03:10:17 -0700

    Fix pass

--- overlays/01-pass.nix
+++ overlays/01-pass.nix
@@ -2,7 +2,7 @@ final: prev:
 {
   pass = with prev; pass.overrideAttrs (old: {
     # Remove emacs package, installed via melpa
-    postInstall = old.postInstall ++ ''
+    postInstall = old.postInstall + ''
       rm -r "$out/share/emacs/site-lisp"
     '';
   });
