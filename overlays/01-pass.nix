7b4440b555e598e0b834aff22e245999dc26cc44
Svend Sorensen <svend@svends.net>
2020-10-29 03:07:09 -0700

    Rename

--- overlays/01-pass.nix
+++ overlays/01-pass.nix
@@ -1,6 +1,6 @@
 final: prev:
 {
-  password-store = with prev; password-store.overrideAttrs (old: {
+  pass = with prev; pass.overrideAttrs (old: {
     # Remove emacs package, installed via melpa
     postInstall = old.postInstall ++ ''
       rm -r "$out/share/emacs/site-lisp"
