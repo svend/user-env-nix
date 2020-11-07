66fd8629156218ee2f1f9b00fca850bbc899dae6
Svend Sorensen <svend@svends.net>
2020-11-06 16:47:06 -0800

    Substitute

--- overlays/systemd.nix
+++ overlays/systemd.nix
@@ -2,10 +2,16 @@ self: super:
 {
   systemdServices = super.runCommand "systemdServices"
     {
+      inherit (self) gitWithConfig isync notmuch;
+      gitAutoCommit = self.myScripts;
+      mbsyncConfig = ../config/isync/mbsyncrc;
+      notmuchConfig = ../config/notmuch/notmuch-config;
       config = ../config/systemd;
     }
     ''
       mkdir -p "$out/etc/xdg/systemd/user"
-      cp "$config"/* "$out/etc/xdg/systemd/user"
+      for f in "$config"/*; do
+         substituteAll "$f" "$out/etc/xdg/systemd/user/$(basename "$f")"
+      done
     '';
 }
