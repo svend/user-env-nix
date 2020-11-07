c3dbddb5d806cbfff8000f1126e1f1ff259c978a
Svend Sorensen <svend@svends.net>
2020-11-06 19:28:09 -0800

    Cleanup unused packages

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,9 +1,7 @@
 self: super:
 {
   display-switch = super.callPackage ../pkgs/display-switch { };
-  # gopls = super.callPackage ../pkgs/gopls { };
   multi-x509 = super.callPackage ../pkgs/multi-x509 { };
-  # rust-analyzer = super.callPackage ../pkgs/rust-analyzer { inherit (prev.darwin.apple_sdk.frameworks) CoreServices; };
   rust-script = super.callPackage ../pkgs/rust-script { };
   rust-scripts = super.callPackage ../pkgs/rust-scripts { };
   scriptisto = super.callPackage ../pkgs/scriptisto { };
