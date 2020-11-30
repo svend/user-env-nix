c992cc6c8b8130d233b00e2dd26cc0656d65998d
Svend Sorensen <svend@svends.net>
2020-11-30 07:52:46 -0800

    Update

--- overlays/linuxEnv.nix
+++ overlays/linuxEnv.nix
@@ -5,7 +5,8 @@ self: super:
     paths = [
       display-switch # FTB darwin: libaio-0.3.111
       gitAndTools.gitAnnex # slow to build, linux has pre-built binaries
-      inetutils
+      # TODO: is inetutils installed by default?
+      # inetutils # telnet, ping, etc.
       isync
       librecad
       notmuchWithConfig # FTB on darwin: gpg: can't connect to the agent: File name too long
