c099f1ceeab5ab796996007fcfadfeb71d228fa1
Svend Sorensen <svend@svends.net>
2021-02-17 15:52:56 -0800

    Bump version

--- overlays/cue.nix
+++ overlays/cue.nix
@@ -7,15 +7,15 @@ self: super: {
     # being overridden
     inherit (super.cue.drvAttrs) pname doCheck subPackages;
     inherit (super.cue) meta;
-    version = "0.3.0-beta.3";
+    version = "0.3.0-beta.5";
 
     src = super.fetchgit {
       url = "https://cue.googlesource.com/cue";
       rev = "v${version}";
-      sha256 = "sha256-2lUlrRleRxWb8kp81+aUNR+HeEelydRMX5VwF1b8RWw=";
+      sha256 = "sha256-H3PCMM7oVsZx6pVveIEeZjv6KcevhSL9NOhh3j3mS0Q=";
     };
 
-    vendorSha256 = "sha256-t03E/KGhwruyF39NJSeHhum3JprTUErMoOe3ixuvuXU=";
+    vendorSha256 = "sha256-9ai1Wbk6ftcXHjVEWaL8drxZnhgAwF8+OXNI95CrNjc=";
 
     buildFlagsArray = [
       "-ldflags=-X cuelang.org/go/cmd/cue/cmd.version=${version}"
