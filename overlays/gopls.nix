9f2abb264a718650622534a052553f18969e984f
Svend Sorensen <svend@svends.net>
2021-02-25 11:07:17 -0800

    Update

--- overlays/gopls.nix
+++ overlays/gopls.nix
@@ -7,12 +7,12 @@ self: super: {
     # being overridden
     inherit (super.gopls.drvAttrs) pname modRoot doCheck subPackages;
     inherit (super.gopls) meta;
-    version = "0.6.5";
+    version = "0.6.6";
     src = super.fetchgit {
       rev = "gopls/v${version}";
       url = "https://go.googlesource.com/tools";
-      sha256 = "sha256-XXEPZyci/x9Hvk4uPo2BFzbGY3YhHggrhrB+EndTTCU=";
+      sha256 = "sha256-1VdkysXhk/GSbmXAFQNMptOvIECo5NKBgAhBhQEhtfM=";
     };
-    vendorSha256 = "sha256-/L0Uqv2Gw156o9uH+EZCWrnmj5MnC6dlFyBr7Wmd418=";
+    vendorSha256 = "sha256-jaHYHYeRaD4uC7H7ufGXefvCHoC+ksY1TRrpfk1nktg=";
   };
 }
