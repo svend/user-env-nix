7a2aa27aabb5a00ad810af74f78aee6d4ae0b775
Svend Sorensen <svend@svends.net>
2021-04-26 10:32:35 -0700

    Update

--- overlays/pythonEnv.nix
+++ overlays/pythonEnv.nix
@@ -5,20 +5,25 @@ self: super:
     name = "pythonEnv";
     ignoreCollisions = true;
     paths = [
-      (with python39Packages; python.buildEnv.override {
+      (with python3Packages; python.buildEnv.override {
         extraLibs = [
           black
           flake8
           isort
+          pipenv
           pylint
+          python-language-server
           pyyaml
           requests
         ];
       })
 
-      python38
+      python39
+      # python38 is the default
       python37
       python36
+
+     # TODO: Can I stop installing python 2?
 
       # 2021-04-26: python 2.7 FTB
       # error: builder for '/nix/store/xsd3gx6zraxcpjdgi5xjyzqa1gcrfpmz-python2.7-importlib_resources-5.1.2.drv' failed with exit code 1;
@@ -30,9 +35,6 @@ self: super:
       #     virtualenv
       #   ];
       # })
-
-      pipenv
-      python-language-server
     ];
   };
 }
