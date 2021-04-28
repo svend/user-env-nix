8d0f4166197bc1175252830c72a8f9fe29dd6ca7
Svend Sorensen <svend@svends.net>
2021-04-27 19:15:53 -0700

    rust: Install gcc on Linux

--- overlays/rustEnv.nix
+++ overlays/rustEnv.nix
@@ -3,17 +3,18 @@ self: super:
   rustEnv = with self; super.buildEnv {
     name = "rustEnv";
     paths = [
-      # Tools required by rustc
-      # gcc
-      # gnumake
-      # binutils-unwrapped
-
       # From rust-overlay
       (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
 
       cargo-edit # cargo add/rm/upgrade (https://github.com/killercup/cargo-edit)
       rust-analyzer
       rust-script
+    ] ++ lib.optionals stdenv.isLinux [
+      # Tools required by rust-overlay on Linux
+      gcc # for `cc`
+      # TODO: Are these required?
+      # gnumake
+      # binutils-unwrapped
     ];
   };
 }
