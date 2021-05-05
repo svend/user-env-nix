bace601a49e0ab11e5233a2489096bd127719266
Svend Sorensen <svend@svends.net>
2021-05-05 09:07:27 -0700

    Remove python 2

--- overlays/pythonEnv.nix
+++ overlays/pythonEnv.nix
@@ -22,19 +22,6 @@ self: super:
       python37
       python36
 
-     # TODO: Can I stop installing python 2?
-
-      # 2021-04-26: python 2.7 FTB
-      # error: builder for '/nix/store/xsd3gx6zraxcpjdgi5xjyzqa1gcrfpmz-python2.7-importlib_resources-5.1.2.drv' failed with exit code 1;
-      # > distutils.errors.DistutilsOptionError: find_namespace: directive is unsupported on Python < 3.3
-
-      # (with python27Packages; python.buildEnv.override {
-      #   extraLibs = [
-      #     pip
-      #     virtualenv
-      #   ];
-      # })
-
       pipenv # pipenv needs to be outside python.buildEnv
     ];
   };
