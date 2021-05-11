73c158dbbaddc068107d35202aa9bea36b4b84f7
Svend Sorensen <svend@svends.net>
2021-05-11 14:03:57 -0700

    emacs: Move font config to config file

--- config/emacs/themes/svend-theme.el
+++ config/emacs/themes/svend-theme.el
@@ -3,7 +3,6 @@
 
 (custom-theme-set-faces
  'svend
- `(default ((t (:height ,(if (eq window-system 'ns) 150 110)))))
  '(nobreak-space ((t (:underline nil))))
  '(sh-heredoc ((t (:inherit font-lock-string-face)))))
 
