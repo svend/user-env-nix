675fbd76a88ff9e6bafbddead3a80a69d3424848
Svend Sorensen <svend@svends.net>
2021-10-29 19:11:17 -0700

    Update

--- pkgs/multi-x509/default.nix
+++ pkgs/multi-x509/default.nix
@@ -10,15 +10,15 @@ buildRustPackage {
     owner = "svend";
     repo = "multi-x509";
     # rev = "${version}";
-    rev = "cef61c7900de156fc24444767b39d05eef2a1a53";
-    sha256 = "sha256-mqQxp1OSy1iEiimKyzDgoRq/0wDtCw+BUhSGGCXUtwI=";
+    rev = "3ba17ef69aa35478914ac22f3cad44ebf46bbbbf";
+    sha256 = "sha256-0qLluFX/C9qKqkgHei9IaIkSMaAGiUXQtaCvHRXVXQc=";
   };
 
   # buildRustPackage requires a cargoSha256 attribute which is computed over all
   # crate sources of this package. Currently it is obtained by inserting a fake
   # checksum into the expression and building the package once. The correct
   # checksum can be then take from the failed build.
-  cargoSha256 = "sha256-STTfmDKFNLWPwpBZ3lq7hSZxaD4Y4cJtj7yaxfp+I+U=";
+  cargoSha256 = "sha256-52jz9EjU+x3dp3OxpHHsk455lK1PHYC2jdGjbByvodw=";
 
   meta = with  lib; {
     description = "Run a command on each x509 certificate";
