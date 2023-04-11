2d7d7add2a8b5d10994e1099a5896200c0eaa0d8
Svend Sorensen <svend@svends.net>
2023-04-11 13:49:07 -0700

    Update d2

--- overlays/d2.nix
+++ overlays/d2.nix
@@ -2,28 +2,27 @@
 # Bug report: https://github.com/NixOS/nixpkgs/issues/86349
 # Workaround: https://discourse.nixos.org/t/inconsistent-vendoring-in-buildgomodule-when-overriding-source/9225/3
 self: super: {
-  # # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/tools/text/d2/default.nix
+  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/tools/text/d2/default.nix
+  d2 = super.buildGoModule rec {
+    # These are the attributes in d2/default.nix, except the ones that are
+    # being overridden
+    inherit (super.d2.drvAttrs) pname nativeBuildInputs postInstall subPackages;
+    inherit (super.d2) meta;
+    version = "0.2.6";
 
-  # d2 = super.buildGoModule rec {
-  #   # These are the attributes in d2/default.nix, except the ones that are
-  #   # being overridden
-  #   inherit (super.d2.drvAttrs) pname nativeBuildInputs postInstall subPackages;
-  #   inherit (super.d2) meta;
-  #   version = "0.2.0";
+    src = super.fetchFromGitHub {
+      owner = "terrastruct";
+      repo = pname;
+      rev = "v${version}";
+      hash = "sha256-bZJu4l5xAVqm/1HIhHfnZF9JRswAE/c6OzuZ8mmHA9U=";
+    };
 
-  #   src = super.fetchFromGitHub {
-  #     owner = "terrastruct";
-  #     repo = pname;
-  #     rev = "v${version}";
-  #     hash = "sha256-1AFioXDIh+qQYhhIaQky2SkGd+amnYdOeoNDU+1+poI=";
-  #   };
+    ldflags = [
+      "-s"
+      "-w"
+      "-X oss.terrastruct.com/d2/lib/version.Version=${version}"
+    ];
 
-  #   ldflags = [
-  #     "-s"
-  #     "-w"
-  #     "-X oss.terrastruct.com/d2/lib/version.Version=${version}"
-  #   ];
-
-  #   vendorSha256 = "sha256-PtKKGHzxshahecbfvvo1nGCseap4o8r8raBpFDUSbx4=";
-  # };
+    vendorSha256 = "sha256-wXE2+a30KohIOuxFeBQPcV7X2Ka+4t7zqHdr48kifY0=";
+  };
 }
