697421ddc3e95a306be2fec247f2fcbe6bdae991
Svend Sorensen <svend@svends.net>
2023-08-23 10:51:54 -0700

    Update

--- overlays/gotools.nix
+++ overlays/gotools.nix
@@ -1,20 +1,9 @@
 self: super:
 {
   gotools = super.gotools.overrideAttrs (old: rec {
-    # Do not install things like doc, link, and fix
-    postConfigure = ''
-      # # Make the builtin tools available here
-      # mkdir -p $out/bin
-      # eval $(go env | grep GOTOOLDIR)
-      # find $GOTOOLDIR -type f | while read x; do
-      #   ln -sv "$x" "$out/bin"
-      # done
-      # export GOTOOLDIR=$out/bin
-    '';
-
     # Do not install bundle (conflicts with ruby bundler)
     excludedPackages = with super; "\\("
-      + pkgs.lib.concatStringsSep "\\|" ([ "bundle" "testdata" "vet" "cover" ])
+      + pkgs.lib.concatStringsSep "\\|" ([ "bundle"  ])
       + "\\)";
   });
 }
