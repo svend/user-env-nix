f14d3e1a0b5b69d499a10b123cfd6e69228759fa
Svend Sorensen <svend@svends.net>
2023-09-05 11:25:45 -0700

    Update comment

--- overlays/goEnv.nix
+++ overlays/goEnv.nix
@@ -3,7 +3,7 @@ self: super:
   goEnv = with self; super.buildEnv {
     name = "goEnv";
     paths = [
-      go_1_21 # TODO: switch to `go` when 1.21 becomes the default version
+      go_1_21 # TODO: switch to `go` when 1.21 becomes the default version (https://github.com/NixOS/nixpkgs/blob/97927dfad9abf6f2d1b3144d069024911a7ecf1f/pkgs/top-level/all-packages.nix#L25999)
 
       # # Legacy dependency management tools
       # dep
