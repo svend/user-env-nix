634d790c6a388c17277dc596c89d2c09a7fad7b2
Svend Sorensen <svend@svends.net>
2023-09-23 19:31:35 -0700

    Remove display-switch, part of OS flake

--- overlays/pkgs.nix
+++ overlays/pkgs.nix
@@ -1,6 +1,5 @@
 self: super:
 {
-  display-switch = super.callPackage ../pkgs/display-switch { };
   multi-x509 = super.callPackage ../pkgs/multi-x509 { };
   rust-script = super.callPackage ../pkgs/rust-script { };
   rust-scripts = super.callPackage ../pkgs/rust-scripts { };
