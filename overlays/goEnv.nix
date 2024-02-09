a98b839d7d7389a62abb72d397d3b511a9c4f7dd
Svend Sorensen <svend@svends.net>
2024-02-09 09:06:45 -0800

    Add comment

--- overlays/goEnv.nix
+++ overlays/goEnv.nix
@@ -3,6 +3,9 @@ self: super:
   goEnv = with self; super.buildEnv {
     name = "goEnv";
     paths = [
+      # Default version of go:
+      # https://search.nixos.org/packages?channel=unstable&show=go&from=0&size=50&sort=relevance&type=packages&query=go
+      # go
       go_1_22
 
       # # Legacy dependency management tools
