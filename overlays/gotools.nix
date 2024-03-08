ebd353331c94ac3170fc84ec2669f0df53a84dab
Svend Sorensen <svend@svends.net>
2024-03-08 11:13:57 -0800

    Build gopls with Go 1.22

--- overlays/gotools.nix
+++ overlays/gotools.nix
@@ -1,8 +1,6 @@
 self: super: {
-  gotools = super.gotools.overrideAttrs (
-    old: rec {
-      # Do not install bundle (conflicts with ruby bundler)
-      excludedPackages = with super; "\\(" + pkgs.lib.concatStringsSep "\\|" ([ "bundle" ]) + "\\)";
-    }
-  );
+  gotools = super.gotools.overrideAttrs (old: rec {
+    # Do not install bundle (conflicts with ruby bundler)
+    excludedPackages = with super; "\\(" + pkgs.lib.concatStringsSep "\\|" ([ "bundle" ]) + "\\)";
+  });
 }
