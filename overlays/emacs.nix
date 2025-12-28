e24471a8a6b972fd784bcb7e27a193cfe3d0cd9a
Svend Sorensen <svend@svends.net>
2025-12-27 16:18:01 -0800

    Fix errors

--- overlays/emacs.nix
+++ overlays/emacs.nix
@@ -26,7 +26,7 @@ final: prev: {
     epkgs:
     [
       final.emacsConfig
-      final.unzip # required for Emacs nov.el package
+      # final.unzip # required for Emacs nov.el package (TODO now generating a conflict, remove this line?)
       final.myPkgs
       epkgs.treesit-grammars.with-all-grammars
     ]
