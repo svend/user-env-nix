8d32390300182e4af808eacb71ff8d362e93301b
Svend Sorensen <svend@svends.net>
2026-04-30 14:07:42 -0700

    Update

--- overlays/gotools.nix
+++ overlays/gotools.nix
@@ -1,6 +1,8 @@
 final: prev: {
   gotools = prev.gotools.overrideAttrs (old: rec {
     # Do not install bundle (conflicts with ruby bundler)
-    excludedPackages = with prev; "\\(" + pkgs.lib.concatStringsSep "\\|" ([ "bundle" ]) + "\\)";
+    # TODO: Do not install modernize (conflicts with gopls)
+    # https://github.com/NixOS/nixpkgs/issues/509480
+    excludedPackages = with prev; "\\(" + pkgs.lib.concatStringsSep "\\|" ([ "bundle" "modernize" ]) + "\\)";
   });
 }
